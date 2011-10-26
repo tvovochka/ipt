43b4aaf388eb803c5948c29c9980ffa5888323b8
Владимир <t_vovochka@mail.ru>
2011-10-26 15:46:00 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipt::Application.config.secret_token = '71bfb076834f65f6b048fa2b7c75983afbb97e1b93b4fe1f7400b696a0c4e14648ad8bcd05e73426167187b953879528def7253aa42f745edea9d78b9b587854'
